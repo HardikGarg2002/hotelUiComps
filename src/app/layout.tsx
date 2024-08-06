bc01112e5c25d5cb7c9df4a1810337c0f61a0dae
hardikgarg2002 <hardikgarg3085@gmail.com>
2024-08-06 14:27:16 +0530

    sidebar in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import SidebarDemo from "@/myComponents/navbars/sidebar";
+import MySidebar from "@/myComponents/navbars/sidebar";
+import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,8 +20,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <SidebarDemo />
-        {children}
+        <div
+          className={cn(
+            "rounded-md flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 w-full flex-1 max-w-7xl mx-auto border border-neutral-200 dark:border-neutral-700 overflow-hidden h-[100vh]"
+          )}
+        >
+          <MySidebar />
+          <div className="flex flex-1">
+            <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
+              {children}
+            </div>
+          </div>
+        </div>
       </body>
     </html>
   );
