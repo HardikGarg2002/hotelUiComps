a55a724852493c2647c6c237dfc1190ca1b57e4e
hardikgarg2002 <hardikgarg3085@gmail.com>
2024-08-09 23:28:20 +0530

    hotel comonent tuned

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,3 +6,8 @@ export default function Home() {
     </>
   );
 }
+
+// https://themeforest.net/category/site-templates/admin-templates?term=hotel
+// https://preview.themeforest.net/item/hotelair-react-hotel-management-admin-dashboard-template/full_screen_preview/53366819?_ga=2.71741269.738470593.1722924828-2056687723.1683356310
+// https://preview.themeforest.net/item/kamr-vite-hotel-admin-dashboard-template/full_screen_preview/52661630?_ga=2.79053906.738470593.1722924828-2056687723.1683356310
+// https://phenomenonstudio.com/projects/tabela-a-management-system-for-restaurants/
--- src/myComponents/table/table.tsx
+++ src/myComponents/table/table.tsx
@@ -1,18 +1,37 @@
+import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
 import { MyPagination } from "./pagination";
 import { MyTableHeader } from "./tableHeader";
 
 export default function MyTable({ data, columns }: any) {
   return (
     <>
-      <MyTableHeader columns={columns} />
-
-      {data.map((hotel: any) => (
-        <div key={hotel.slug}>
-          <h1>{hotel.title}</h1>
-          <p>{hotel.description}</p>
-        </div>
-      ))}
-
+      <div className="rounded-xl border my-3">
+        <Table>
+          <MyTableHeader columns={columns} />
+          <TableBody>
+            {data.length ? (
+              data.map((row: any) => (
+                <TableRow key={row.slug}>
+                  {columns.map((column: any) => (
+                    <TableCell key={column.accessor}>
+                      {row[column.accessor]}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
       <MyPagination totalCount={(data as any[]).length} />
     </>
   );
