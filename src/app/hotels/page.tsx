10753a5cdd6f4cd0fadfc96c39f8a595f247ddbd
hardikgarg2002 <hardikgarg3085@gmail.com>
2024-08-10 23:35:13 +0530

    made pagination a independent component

--- src/app/hotels/page.tsx
+++ src/app/hotels/page.tsx
@@ -21,9 +21,20 @@ const columns = [
   },
 ];
 
-const HotelsPage = async () => {
+const HotelsPage = async ({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) => {
+  const page = searchParams["page"] ?? "1";
+  const per_page = searchParams["per_page"] ?? "5";
+
+  // mocked, skipped and limited in the real app
+  const start = (Number(page) - 1) * Number(per_page); // 0, 5, 10 ...
+  const end = start + Number(per_page); // 5, 10, 15 ...
+
   const hotels = await getAllHotels();
-  const updatedHotels = hotels.map((hotel: any) => ({
+  const updatedHotels = hotels.slice(start, end).map((hotel: any) => ({
     ...hotel,
     address: hotel.address.street,
   }));
