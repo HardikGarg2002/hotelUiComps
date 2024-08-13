5f8503a52d72f3c02b5fda6a7e4b4e3ef5b9b1c0
hardikgarg2002 <hardikgarg3085@gmail.com>
2024-08-13 16:39:01 +0530

    restaurant table

---
+++ src/interface/restaurant.ts
@@ -0,0 +1,36 @@
+import { IContactInfo } from "./hotel";
+
+export interface IRestaurant {
+  _id?: string;
+  slug: string;
+  hotel_slug: string;
+  title: string;
+  restaurant_type: RestaurantType;
+  // location: string; location will be the same as hotel_slug
+  contact_info: IContactInfo;
+  images: string[];
+  opening_hours: string;
+  menu: IMenuItem[]; // menu will be a separate collection
+  floors: {
+    floor_number: number;
+    tables: {
+      table_number: number;
+      seats: number;
+      status: "AVAILABLE" | "RESERVED" | "OCCUPIED";
+    }[];
+  }[];
+}
+
+export enum RestaurantType {
+  CAFE = "CAFE",
+  LOUNGE = "LOUNGE",
+  BAR = "BAR",
+  RESTAURANT = "RESTAURANT",
+}
+
+export interface IMenuItem {
+  title: string;
+  price: number;
+  description: string;
+  images: string[];
+}
