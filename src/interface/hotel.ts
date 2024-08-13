318a81d0d7e85e6f83f6cb3b96b353016f213d54
hardikgarg2002 <hardikgarg3085@gmail.com>
2024-08-13 17:46:18 +0530

    hotel interface

---
+++ src/interface/hotel.ts
@@ -0,0 +1,36 @@
+// import { ICustomer } from "./customer";
+
+// This hotel will also work as restaurant. So, we can book a table in the hotel.
+export interface IHotel {
+  _id?: string;
+  slug: string;
+  title: string;
+  desc: string;
+  address: IAddress;
+  contact_info: IContactInfo;
+  website: string;
+  associated_branches: IHotel["slug"][]; // slug
+  images: string[];
+  amenities: string[];
+}
+
+export interface IAddress {
+  street: string;
+  city: string;
+  state: string;
+  zip: string;
+  country: string;
+}
+
+export interface IContactInfo {
+  phone: string;
+  email: string;
+}
+
+export interface Reservation {
+  res_no: number;
+  //   customer: ICustomer;
+  // room: IRoom;
+  checkInDate: Date;
+  checkOutDate: Date;
+}
